6ef052e57bb1ce0285050c6a15fcb891e800aa49
vuhieuminh2597 <vuhieuminh2597@gmail.com>
2024-01-03 16:34:34 +0700

    version 1.0.1 SNAPSHOT 6

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,12 +1,12 @@
 import axios from "./customize-axios";
 
 const fetchAllUser = (page) => {
-    return axios.get(`/api/user?page=${page}`)
+    return axios.get(`/api/v1/customer?page=${page}`)
 }
 const postCreateUser = (name, email,birthDay,address,gender,phone) => {
-    return axios.post('/api/user/create',{name, email,birthDay,address,gender,phone});
+    return axios.post('/api/v1/customer/create',{name, email,birthDay,address,gender,phone});
 }
 const putUpdateUser = (id,name, email,birthDay,address,gender,phone) =>{
-    return axios.put('/api/user/update',{id,name, email,birthDay,address,gender,phone})
+    return axios.put('/api/v1/customer/update',{id,name, email,birthDay,address,gender,phone})
 }
 export {fetchAllUser,postCreateUser,putUpdateUser};
