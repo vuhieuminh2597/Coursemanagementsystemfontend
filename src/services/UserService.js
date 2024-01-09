aa5f76efb6b3350dd23c7276c549dfd557a121b3
vuhieuminh2597 <146744092+vuhieuminh2597@users.noreply.github.com>
2024-01-09 21:56:32 +0700

    Update UserService.js

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,16 +1,16 @@
 import axios from "./customize-axios";
 
 const fetchAllUser = (page) => {
-    return axios.get(`/api/v1/customer?page=${page}`)
+    return axios.get(`/api/v1/customers?page=${page}`)
 }
 const postCreateUser = (id,name, email,birthDay,address,gender,phone) => {
-    return axios.post('/api/v1/customer/create',{id,name, email,birthDay,address,gender,phone});
+    return axios.post('/api/v1/customers/create',{id,name, email,birthDay,address,gender,phone});
 }
 const putUpdateUser = (id,name, email,birthDay,address,gender,phone) =>{
-    return axios.put('/api/v1/customer/update',{id,name, email,birthDay,address,gender,phone})
+    return axios.put('/api/v1/customers/update',{id,name, email,birthDay,address,gender,phone})
 }
 
 const deleteUser = (id) => {
-  return axios.delete(`/api/v1/customer/delete/${id}`);
+  return axios.delete(`/api/v1/customers/delete/${id}`);
 }
 export {fetchAllUser,postCreateUser,putUpdateUser,deleteUser};
