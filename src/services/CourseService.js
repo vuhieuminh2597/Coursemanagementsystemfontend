e7764710976519cebbba539b1f3656cb28a0a9ee
vuhieuminh2597 <146744092+vuhieuminh2597@users.noreply.github.com>
2024-01-09 21:58:34 +0700

    Update CourseService.js

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -1,11 +1,11 @@
 import axios from "./customize-axios";
 
 const fetchAllCourses = (page) => {
-    return axios.get(`/api/v1/course?page=${page}`)
+    return axios.get(`/api/v1/courses?page=${page}`)
 }
 
 const registrationCourse = (idUser,idCourse) => {
-    return axios.put(`/api/v1/customer/${idUser}/course/${idCourse}`);
+    return axios.put(`/api/v1/customers/${idUser}/course/${idCourse}`);
 }
 
 export {fetchAllCourses,registrationCourse}
